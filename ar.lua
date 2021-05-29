139292b2fdf4aa08581bf17e3d533608d7204b60
WrightTrae <trae.wright12@gmail.com>
2021-05-29 18:55:57 -0400

    init

---
+++ ar.lua
@@ -0,0 +1,11 @@
+controller = peripheral.wrap("left") -- Defines the controller to the left of the computer
+controller.setRelativeMode(true, 1600, 900) -- Convenient Aspect ratio for most screens
+while true do
+  local timer = os.startTimer(1)
+  local event, id
+  repeat
+    event, id = os.pullEvent("timer")
+  until id == timer
+  controller.clear() -- If you don't do this, the texts will draw over each other
+  controller.drawRightboundString(os.date(), -10, 10, 0xffffff)
+end
